58ba18f28729ff6da9e4c965cc6a4ae3fcfd089d
Serhii Antoniuk <sergiomatiash0@gmail.com>
2022-04-23 22:40:06 +0300

    V.1.0.3 - FORTH COMMIT excluding modularization

--- app/src/main/java/com/example/cleanarhitecturekoinkotlinattemptfive/presenter/MainActivity.kt
+++ app/src/main/java/com/example/cleanarhitecturekoinkotlinattemptfive/presenter/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.cleanarhitecturekoinkotlinattemptfive.presenter
 
-import android.app.Activity
+
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -10,26 +10,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.cleanarhitecturekoinkotlinattemptfive.R
-import com.example.cleanarhitecturekoinkotlinattemptfive.data.repository.UserRepositoryImpl
-import com.example.cleanarhitecturekoinkotlinattemptfive.data.storage.sharedprefs.SharedPrefUserStorage
-import com.example.cleanarhitecturekoinkotlinattemptfive.domain.models.SaveUserNameParam
-import com.example.cleanarhitecturekoinkotlinattemptfive.domain.models.UserName
-import com.example.cleanarhitecturekoinkotlinattemptfive.domain.usecase.GetUserNameUseCase
-import com.example.cleanarhitecturekoinkotlinattemptfive.domain.usecase.SaveUserNameUseCase
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity: AppCompatActivity() {
 
 
 
 
-    private lateinit var vm: MainVIewModel
+    private val vm by viewModel<MainVIewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.e("AAA", "onCreate: Activity created" )
-        vm = ViewModelProvider(this, MainViewModelFactory(this)).get(MainVIewModel::class.java)
+
 
         val dataTextView = findViewById<TextView>(R.id.dataTextView)
         val dataEditView = findViewById<EditText>(R.id.dataEditText)
